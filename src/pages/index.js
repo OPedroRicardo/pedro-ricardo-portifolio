5b2805421cf67ed7e2fa1f544af9a9e755fb99be
Pedro Ricardo <ordepricardo13@gmail.com>
2023-04-11 12:18:33 -0300

    fix(page's title)

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,20 +58,19 @@ export default function Home() {
     <>
       <Head>
         <title>{ title }</title>
-        <title>{ description }</title>
         <meta name="title" content={title} />
-        <meta name="description" content={title}/>
+        <meta name="description" content={description}/>
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://pedro-ricardo-portifolio.vercel.app/" />
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={title} />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="./sitepscr" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://pedro-ricardo-portifolio.vercel.app/" />
         <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={title} />
+        <meta property="twitter:description" content={description} />
         <meta property="twitter:image" content="./siteprsc" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
